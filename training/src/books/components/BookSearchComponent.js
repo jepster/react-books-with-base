10c31290ac560fc8a60b68d55c66d28024190bcf
Peter Majmesku <majmesku@publicplan.de>
2022-08-31 10:11:50 +0200

    Log on search

--- training/src/books/components/BookSearchComponent.js
+++ training/src/books/components/BookSearchComponent.js
@@ -1,19 +1,20 @@
-import { booksRepository } from "../ApplicationContext"
+import {booksRepository, whiteboard} from "../ApplicationContext"
 import BookComponent from "./BookComponent"
 import { useState } from "react"
 export default function BooksSearchComponent() {
     const handleSubmit = (event) => {
         event.preventDefault()
         setBook(booksRepository.findByIsbn(isbn))
+        whiteboard.create.next("Searched for book isbn number: " + isbn)
     }
     const [isbn, setIsbn] = useState("");
     const [book, setBook] = useState(null);
         return (
-            <>  
+            <>
             <form onSubmit={handleSubmit}>
                 <label>ISBN:
                 <input
-                    type="text" 
+                    type="text"
                     value={isbn}
                     onChange={(e) => setIsbn(e.target.value)}
                 />
